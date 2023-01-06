b0cf7c38f3df95aaaa1724d9941a12356d79ad38
Aditya Agarwal <99405991+QMiNt@users.noreply.github.com>
2023-01-07 03:59:04 +0530

    rework2.0

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,41 +1,48 @@
-import React , {useState} from 'react'
-import {Link,useNavigate} from 'react-router-dom'
-import {CustomButton} from './'
-import {logo,menu,search,thirdweb} from '../assets'
-import { navlinks } from '../constants'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+
+import { useStateContext } from '../context';
+import { CustomButton } from './';
+import { logo, menu, search, thirdweb } from '../assets';
+import { navlinks } from '../constants';
+
 const Navbar = () => {
   const navigate = useNavigate();
-  const [isActive,setIsActive] = useState ('dashboard');
-  const [toggleDrawer,setToggleDrawer] = useState(false);
-  const address='0xabc'
+  const [isActive, setIsActive] = useState('dashboard');
+  const [toggleDrawer, setToggleDrawer] = useState(false);
+  const { connect, address } = useStateContext();
+
   return (
-    <div className='flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6'> 
-    <div className='lg:flex-1 flex flex-row max-w-[458px] py-2 pr-2 h-[52] bg-[#ffffff] rounded-[100px]'>
-      <input type="text" placeholder='Search for campaigns' className='flex w-full font-epilogue font-normal text-[14px] placeholder:text-white bg-transparent outline-none'/>
-      <div className='w-[72px] h-full rounded-[20px] bg-[#367cff] flex justify-center items-center cursor-pointer]'>
-<img src={search} alt="search"
-className='w-[15px] h-[15px] object-contain' />
-      </div>
-      </div>
-      <div className='sm:flex hidden flex-row justify-end gap-4' >
-    <CustomButton
-    btn type="button"
-    title={address ? 'Create a campaign ' : 'Connect'}
-    styles={address ? 'bg-[#1dc071]' : 'bg-[#8c6dfd]'}
-    handleClick={() =>{
-    if(address) navigate('create-campaign')
-  else 'connect()' }}
-    />
-    <Link to="/profile">
-      <div className='w-[52px] h-[52px] rounded-full bg-[#367cff] flex justify-center items-center cursor-pointer'>
-        <img src={thirdweb} alt="user" className="w-[60%] h-[60%] object-contain"/>
+    <div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
+      <div className="lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] bg-[#ffffff] rounded-[100px]">
+        <input type="text" placeholder="Search for campaigns" className="flex w-full font-epilogue font-normal text-[14px] placeholder:text-[#4b5264] text-white bg-transparent outline-none" />
+        
+        <div className="w-[72px] h-full rounded-[20px] bg-[#376cff] flex justify-center items-center cursor-pointer">
+          <img src={search} alt="search" className="w-[15px] h-[15px] object-contain"/>
+        </div>
       </div>
 
-    </Link>
+      <div className="sm:flex hidden flex-row justify-end gap-4">
+        <CustomButton 
+          btnType="button"
+          title={address ? 'Create a campaign' : 'Connect'}
+          styles={address ? 'bg-[#376cff]' : 'bg-[#376cff]'}
+          handleClick={() => {
+            if(address) navigate('create-campaign')
+            else connect()
+          }}
+        />
+
+        <Link to="/profile">
+          <div className="w-[52px] h-[52px] rounded-full bg-[#ffffff] flex justify-center items-center cursor-pointer">
+            <img src={thirdweb} alt="user" className="w-[60%] h-[60%] object-contain" />
+          </div>
+        </Link>
       </div>
-{/*small screen*/}
-<div className="sm:hidden flex justify-between items-center relative">
-        <div className="w-[40px] h-[40px] rounded-[10px] bg-[#367cff] flex justify-center items-center cursor-pointer">
+
+      {/* Small screen navigation */}
+        <div className="sm:hidden flex justify-between items-center relative">
+        <div className="w-[40px] h-[40px] rounded-[10px] bg-[#2c2f32] flex justify-center items-center cursor-pointer">
             <img src={logo} alt="user" className="w-[60%] h-[60%] object-contain" />
           </div>
 
@@ -46,7 +53,7 @@ className='w-[15px] h-[15px] object-contain' />
             onClick={() => setToggleDrawer((prev) => !prev)}
           />
 
-          <div className={`absolute top-[60px] right-0 left-0 bg-[#567dcd] z-10 shadow-secondary py-4 ${!toggleDrawer ? '-translate-y-[100vh]' : 'translate-y-0'} transition-all duration-700`}>
+          <div className={`absolute top-[60px] right-0 left-0 bg-[#1c1c24] z-10 shadow-secondary py-4 ${!toggleDrawer ? '-translate-y-[100vh]' : 'translate-y-0'} transition-all duration-700`}>
             <ul className="mb-4">
               {navlinks.map((link) => (
                 <li
@@ -63,7 +70,7 @@ className='w-[15px] h-[15px] object-contain' />
                     alt={link.name}
                     className={`w-[24px] h-[24px] object-contain ${isActive === link.name ? 'grayscale-0' : 'grayscale'}`}
                   />
-                  <p className={`ml-[20px] font-epilogue font-semibold text-[14px] ${isActive === link.name ? 'text-[#ffffff]' : 'text-[#86b5f0]'}`}>
+                  <p className={`ml-[20px] font-epilogue font-semibold text-[14px] ${isActive === link.name ? 'text-[#1dc071]' : 'text-[#808191]'}`}>
                     {link.name}
                   </p>
                 </li>
@@ -74,7 +81,7 @@ className='w-[15px] h-[15px] object-contain' />
             <CustomButton 
               btnType="button"
               title={address ? 'Create a campaign' : 'Connect'}
-              styles={address ? 'bg-[#376CFF]' : 'bg-[#376CFF]'}
+              styles={address ? 'bg-[#1dc071]' : 'bg-[#8c6dfd]'}
               handleClick={() => {
                 if(address) navigate('create-campaign')
                 else connect();
@@ -83,8 +90,7 @@ className='w-[15px] h-[15px] object-contain' />
             </div>
           </div>
         </div>
-      </div>
-      
+    </div>
   )
 }
 
