3ae15c1c5475689e1261cf758ba6e90c7466680b
Aditya Agarwal <99405991+QMiNt@users.noreply.github.com>
2023-01-06 17:18:50 +0530

    clientSide Work phase 1

---
+++ client/src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {BrowserRouter as Router} from 'react-router-dom'
+import {ChainId, ThirdwebProvider} from '@thirdweb-dev/react'
+import App from './App'
+import './index.css'
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+ <ThirdwebProvider desiredChainId={ChainId.Mumbai}>
+    <Router>
+        <App/>
+    </Router>
+ </ThirdwebProvider>   
+)
